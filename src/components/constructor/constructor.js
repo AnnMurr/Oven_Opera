5b392a27494187f52ab7fcc87516d9869e84dac1
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-01-05 19:04:36 +0200

    added remove aand add ingredients functionality

--- src/components/constructor/constructor.js
+++ src/components/constructor/constructor.js
@@ -1,6 +1,9 @@
 import { getFormatCurrency } from "../../core/utils/formatCurrency.js";
 
 const constructorAddBtn = document.querySelectorAll(".constructor__add-btn");
+const constructorDeleteBtn = document.querySelectorAll(
+  ".constructor__delete-btn"
+);
 const constructorCards = document.querySelector(".constructor__cards");
 const constructorSize = document.querySelector(".constructor__size");
 
@@ -8,12 +11,16 @@ constructorAddBtn.forEach((btn) =>
   btn.addEventListener("click", addIngredients)
 );
 
+constructorDeleteBtn.forEach((btn) =>
+  btn.addEventListener("click", removeIngredients)
+);
+
 constructorSize.addEventListener("change", changeSize);
 
 const sizePrice = {
-  small: 6.79,
-  medium: 7.89,
-  large: 8.99,
+  small: "€6.79",
+  medium: "€7.89",
+  large: "€8.99",
 };
 
 function addSize() {
@@ -34,10 +41,28 @@ function changeSize(event) {
 
 function addIngredients(event) {
   const btn = event.target;
+  const deleteBtn = event.target.parentNode.nextElementSibling;
   const title = btn.parentNode.parentNode.children[0].children[0].textContent;
   const price = btn.parentNode.parentNode.children[1].children[0].textContent;
 
+  btn.parentNode.classList.remove("constructor__add_active");
+  deleteBtn.classList.add("constructor__delete_active");
   constructorCards.append(createCard(title, price));
+}
+
+function removeIngredients(event) {
+  const btn = event.target;
+  const addBtn = event.target.parentNode.previousElementSibling;
+  const cardName = document.querySelectorAll(".card__name");
+  const itemName = btn.parentNode.parentNode.children[0].children[0];
+
+  const card = Array.from(cardName).filter(
+    (name) => name.children[0].textContent === itemName.textContent
+  )[0];
+
+  card.parentNode.parentNode.remove();
+  btn.parentNode.classList.remove("constructor__delete_active");
+  addBtn.classList.add("constructor__add_active");
 }
 
 function createCard(title, price) {
@@ -70,7 +95,7 @@ function createCardPrice(_, price) {
   const container = document.createElement("div");
   container.classList.add("card__price");
   const priceText = document.createElement("span");
-  priceText.textContent = getFormatCurrency(price);
+  priceText.textContent = price;
 
   container.append(priceText);
   return container;
